e63315e8767aa8a84a130f14850c1bb29730e5ce
Anapplewj <2648175705@qq.com>
2021-03-04 21:45:00 +0800

    逆波兰练习~

--- src/Pra0225/While.java
+++ src/Pra0225/While.java
@@ -1,12 +1,24 @@
 package Pra0225;
-
-public class While {
-    public void loop(){
-        int x=10;
-        while(10){
-            System.out.println("haha"+(x-1));
-            x-=1;
-        }
+public class While{
+    public static void main(String[] args) {
+//        String str1="11";
+//        String str2="1";
+//        System.out.println(Integer.toBinaryString(Integer.parseInt(str1,2)+Integer.parseInt(str2,2)));
+//        double d=Math.cos(42);//错误
+        double d=Math.cos(Math.toDegrees(42));
+        System.out.println(d);
+        float []f[]=new float[3][];
+        float[][] f2=new float[2][2];
+//        float f3[][]=new float[][2]; //错误
     }
 }
+//public class While {
+//    public void loop(){
+//        int x=10;
+//        while(10){
+//            System.out.println("haha"+(x-1));
+//            x-=1;
+//        }
+//    }
+//}
 //上面的代码中，第六行有误，布尔类型
